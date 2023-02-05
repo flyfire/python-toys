4f789c9e121b2e135c2a87349846278069014458
Solarex <rh.hou.work@gmail.com>
2023-02-05 20:02:49 +0800

    test cover identify

---
+++ test_cover_identify.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def test():
+    headers = {"Accept": "application/json",
+               "device_type": "m3", "device_id": "11691500272904",
+               "device_sign": "",
+               "User-Agent": "okhttp/3.12.0", "Connection": "keep-alive",
+               "Host": "119.23.148.25:9900",
+               "Accept-Encoding": "gzip"
+               }
+    url = "http://119.23.148.25:9900/paipai/coverIdentify"
+    files = {'file': ("cover-crop.jpeg", open("cover-crop.jpeg", "rb"), "image/jpeg", {})}
+    res = requests.request("POST", url, data=None, files=files, headers=headers)
+    print("status code " + str(res.status_code) + "\n")
+    print("response " + res.text + "\n")
+    print("******************************\n")
+
+
+if __name__ == '__main__':
+    test()
